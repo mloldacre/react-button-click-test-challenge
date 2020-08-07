e7a38a3b077096b079691f4e6b7d7851e53483fc
Michael Oldacre <mloldacre@live.com>
2020-08-06 20:58:22 -0400

    Updated test

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -5,7 +5,7 @@ import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const wrapper = shallow(<Button onClick={() => console.log('Click')}/>);
         wrapper.find('button').simulate('click');
     });
 });
